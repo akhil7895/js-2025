b8c8affe5a16f21d7c202099ea19becf6b38e916
Akhil chaturvedi <128232661+akhil7895@users.noreply.github.com>
2025-01-06 16:34:04 +0000

    dataTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const name = "Akhil Chaturvedi"
+
+let emailId = "chaturvediakhil17@gmail.com"
+
+let userAdd = "Auraiya "
+
+console.table([name,emailId,userAdd])
+
+
+console.log(typeof(userAdd))
+console.log(typeof userAdd )
+console.log(typeof null)
