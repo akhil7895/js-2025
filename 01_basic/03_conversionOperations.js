7cf04d2ed3d5e3da81f96903d3b513f4f3de9b93
Akhil chaturvedi <128232661+akhil7895@users.noreply.github.com>
2025-01-06 16:53:43 +0000

    conversion of db

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = 20
+
+console.log(typeof score)
+
+let score2 = "20abc"
+
+console.log(typeof score2)
+
+let score2InNumber = Number(score2)
+
+console.log(typeof score2InNumber)
+
+console.log(score2InNumber)
+
+console.log(typeof NaN)
+
+// 1 = true; 0 → false
+// "0" => false
+// "hitesh" => true
